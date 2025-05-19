a06ebcda90c4703a25f4b7df2027680daf64a709
João Pedro Alves <joaopedroab20147@gmail.com>
2025-05-18 21:43:53 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
+import React from 'react';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -14,16 +14,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <div className="flex flex-col min-h-screen">
-          <div className="flex-grow">
-            <Routes>
-              <Route path="/ecommerce" element={<Index />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
-        </div>
-      </BrowserRouter>
+      <React.StrictMode>
+        <Index />
+      </React.StrictMode>
     </TooltipProvider>
   </QueryClientProvider>
 );
